bf66cd1f5985ba5bda46419c67a73b32d45e9922
M Fonseka <f201658@heb.com>
2020-10-26 12:37:57 -0500

    combining everything together

--- countdown-dash/src/App.js
+++ countdown-dash/src/App.js
@@ -11,7 +11,7 @@ function App() {
 
       const fetchUsers = async () => {
         try {
-          const response = await axios.get("http://192.168.0.16:8080");
+          const response = await axios.get("http://192.168.0.16:8080/tasks");
           const items = []
           response.data.map((v,i)=>{
 
